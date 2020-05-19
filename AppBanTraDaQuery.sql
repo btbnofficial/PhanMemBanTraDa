4d8132383989c44375b32c072533dd0c182ed3ed
btbnofficial@gmail.com <csdesigner>
2020-05-19 17:56:33 +0700

    sua database

--- AppBanTraDaQuery.sql
+++ AppBanTraDaQuery.sql
@@ -1,38 +1,38 @@
-create database AppBanTraDa;
+﻿create database AppBanTraDa;
 go
 use AppBanTraDa;
 go
 
-create table Food
+Create table Food
 (
 	Id int identity primary key,
-	Name nvarchar(100),
-	Price float
+	Name nvarchar(100) not null default N'Chưa đặt tên',
+	Price float not null default 0
 );
 
 create table TableFood
 (
 	Id int identity primary key,
-	Status nvarchar(100),
-	Name nvarchar(100)
+	Status nvarchar(100) not null default N'Trống',
+	Name nvarchar(100) not null default N'Chưa đặt tên'
 );
 
 create table Bill
 (
 	Id int identity primary key,
-	DateCheckIn Date,
+	DateCheckIn Date not null default getDate(),
 	DateCheckOut Date,
-	TableId int,
-	Status int, --Da thanh toan: 1, Chua thanh toan: 0
+	TableId int not null,
+	Status int not null, --Da thanh toan: 1, Chua thanh toan: 0
 	foreign key(TableId) references dbo.TableFood(Id)
 );
 
 create table BillDetail
 (
 	Id int identity primary key,
-	BillId int,
-	FoodId int,
-	count int,
+	BillId int not null,
+	FoodId int not null,
+	count int not null,
 	foreign key(BillId) references dbo.Bill(Id),
 	foreign key(FoodId) references dbo.Food(Id)
 );
@@ -40,6 +40,6 @@ create table BillDetail
 create table Account
 (
 	Username varchar(100) primary key,
-	Password varchar(100),
-	Type int --1: admin, 0: staff
+	Password varchar(100) not null,
+	Type int not null --1: admin, 0: staff
 );
